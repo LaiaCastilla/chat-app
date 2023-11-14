c5aa8c4eb4cb4fa3ef6be6114f9e84c8598818d1
Laia Castilla <lcastillaiamoros@gmail.com>
2023-11-14 14:23:15 +0100

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import GoogleSignIn from "../img/btn_google_signin_dark_pressed_web.png";
+
+function NavBar() {
+  const [user, setUser] = useState(false);
+
+  const googleSignIn = () => {
+    setUser(true);
+  };
+
+  const signOut = () => {
+    setUser(false);
+  };
+
+  return (
+    <nav className="nav-bar">
+      <h1>React chat</h1>
+      {user ? (
+        <button className="sign-out" onClick={signOut} type="button">
+          Sign out
+        </button>
+      ) : (
+        <button className="sign-in">
+          <img
+            onClick={googleSignIn}
+            src={GoogleSignIn}
+            alt="Sign in with Google"
+            type="button"
+          />
+        </button>
+      )}
+    </nav>
+  );
+}
+
+export default NavBar;
