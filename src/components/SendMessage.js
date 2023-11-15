55f7923829dff2230681e055eb55feca8af73963
Laia Castilla <lcastillaiamoros@gmail.com>
2023-11-15 18:00:04 +0100

    update sendmessage

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 function SendMessage() {
+  const [message, setMessage] = useState("");
+
+  const SendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      alert("Enter a valid message");
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    setMessage("");
+  };
+
   return (
-    <form className="send-message">
+    <form onSubmit={(event) => SendMessage(event)} className="send-message">
       <label htmlFor="messageInput" hidden>
         Enter message
       </label>
@@ -10,8 +31,10 @@ function SendMessage() {
         id="messageInput"
         name="messageInput"
         type="text"
+        value={message}
         className="form-input__input"
         placeholder="type your message..."
+        onChange={(event) => setMessage(event.target.value)}
       />
       <button type="submit">Send</button>
     </form>
